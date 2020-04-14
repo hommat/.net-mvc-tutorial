0181b057d571acfefb268259dd0465bad26524b5
hommat <zmateusz1111@gmail.com>
2020-04-14 13:52:17 +0200

    Part 3 - add a model

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,9 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(
+                options => options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
